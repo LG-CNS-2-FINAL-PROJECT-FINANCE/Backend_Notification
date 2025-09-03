38589e41ebfcc29a225910ebc2cef56fcca22846
5ohmydays5 <ooojjhn5@gmail.com>
2025-09-03 21:43:48 +0900

    Refactor: SendNotification Test

--- src/main/java/com/ddiring/Backend_Notification/kafka/NotificationConsumer.java
+++ src/main/java/com/ddiring/Backend_Notification/kafka/NotificationConsumer.java
@@ -1,11 +1,14 @@
 package com.ddiring.Backend_Notification.kafka;
 
 import com.ddiring.Backend_Notification.service.NotificationService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -19,8 +22,9 @@ public class NotificationConsumer {
     @KafkaListener(topics = MAIN_TOPIC, groupId = "notification-group")
     public void consume(String message) {
         try {
-//            log.info(message);
-            NotificationEvent event = objectMapper.readValue(message, NotificationEvent.class);
+            Map<String, Object> messageMap = objectMapper.readValue(message, new TypeReference<>() {});
+
+            NotificationEvent event = objectMapper.convertValue(messageMap, NotificationEvent.class);
             notificationService.handleNotificationEvent(event);
         } catch (Exception e) {
             log.error("Consumer 처리 실패: message={}, error={}", message, e.getMessage(), e);
