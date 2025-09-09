c83e445a149ec825938fdb6892277fd0266b0fa1
5ohmydays5 <ooojjhn5@gmail.com>
2025-09-09 10:35:57 +0900

    Refactor: fcm 알림 추가

--- src/main/java/com/ddiring/Backend_Notification/config/FirebaseConfig.java
+++ src/main/java/com/ddiring/Backend_Notification/config/FirebaseConfig.java
@@ -20,27 +20,34 @@ public class FirebaseConfig {
     public FirebaseApp firebaseApp() throws IOException {
         String firebaseJson = System.getenv("FIREBASE_CONFIG");
 
-        if (firebaseJson != null && !firebaseJson.isEmpty()) {
-            //운영 환경: 환경변수에서 읽기
-            GoogleCredentials credentials = GoogleCredentials.fromStream(
-                    new ByteArrayInputStream(firebaseJson.getBytes())
-            );
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(credentials)
-                    .build();
-            return FirebaseApp.initializeApp(options);
-        } else {
-            //로컬 테스트: 파일에서 읽기
-            String localPath = "src/main/resources/serviceAccountKey.json"; //다운로드한 JSON 위치
-            if (!Files.exists(Paths.get(localPath))) {
-                throw new IllegalStateException("Firebase 설정 파일 없음: " + localPath);
-            }
-            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(localPath));
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(credentials)
-                    .build();
-            return FirebaseApp.initializeApp(options);
-        }
+//        if (firebaseJson != null && !firebaseJson.isEmpty()) {
+//            //운영 환경: 환경변수에서 읽기
+//            GoogleCredentials credentials = GoogleCredentials.fromStream(
+//                    new ByteArrayInputStream(firebaseJson.getBytes())
+//            );
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(credentials)
+//                    .build();
+//            return FirebaseApp.initializeApp(options);
+//        } else {
+//            //로컬 테스트: 파일에서 읽기
+//            String localPath = "src/main/resources/serviceAccountKey.json"; //다운로드한 JSON 위치
+//            if (!Files.exists(Paths.get(localPath))) {
+//                throw new IllegalStateException("Firebase 설정 파일 없음: " + localPath);
+//            }
+//            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(localPath));
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(credentials)
+//                    .build();
+//            return FirebaseApp.initializeApp(options);
+//        }
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ByteArrayInputStream(firebaseJson.getBytes())
+        );
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+        return FirebaseApp.initializeApp(options);
     }
 
     @Bean
